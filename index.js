f199cf2200a8c95476b90e2532e92e310ac36d7b
DanielSharp01 <DanielSharp01@gmail.com>
2019-03-14 02:53:44 +0100

    TRAIN API parsed

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-let express = require("express");
-let app = express();
+const express = require("express");
+const app = express();
 var fs = require("fs");
 
-let cheerio = require("cheerio");
 const api = require("./mavapi");
-const { parseTrainStations, parseTrainHeader } = require("./parser/train-parser");
-const TrainInfoStatement = require("./parser/statements");
+const TrainParser = require("./parser/train-parser");
 
 app.use(express.static("./public"));
 
-app.get("/:id", (req, res, next) => {
+app.get("/train/:id", (req, res, next) => {
+  res.header("Content-Type", "text/html; charset=utf-8");
   api({
     a: "TRAIN",
     jo: {
@@ -17,39 +16,13 @@ app.get("/:id", (req, res, next) => {
     }
   })
     .then(apiRes => {
-      const $ = cheerio.load(apiRes.d.result.html, { decodeEntities: true });
-      res.header("Content-Type", "text/html; charset=utf-8");
+      let tp = new TrainParser(apiRes);
 
-      // parseTrainStations($);
-
-      // res.send(
-      //   $("th.title")
-      //     .first()
-      //     .html() + "<br>"
-      // );
-
-      res.send(parseTrainHeader($));
-
-      //res.end($.html({ decodeEntities: false }));
+      tp.run();
+      res.send(tp.ch.html());
     })
     .catch(err => console.error(err));
 });
-
-// let testObj = { a: "STATION", jo: { a: "Monor" } };
-
-// let testObj = {
-//   a: "TRAIN",
-//   jo: {
-//     vsz: "55347"
-//   }
-// };
-
-// let testObj = {
-//   a: "TRAIN",
-//   jo: {
-//     vsz: "552648"
-//   }
-// };
 
 app.listen(3000, () => {
   console.log(":3000");
--- parser/train-parser.js
+++ parser/train-parser.js
@@ -6,100 +6,127 @@ const {
   trainRelation,
   trainStationInfo,
   trainStationLink,
-  trainInstance
+  trainElviraId
 } = require("./statements");
+
 const processStatement = require("./process-statement");
+const cheerio = require("cheerio");
 
-const trainNum = 1;
-
-function parseTrainHeader($) {
-  let header = $("th.title").first();
-  let contents = header.contents();
-  let textNode = contents.eq(0).text();
-  let words = textNode.split(" ").map(w => w.trim());
-  let number = parseInt(words[0]);
-  let name;
-  let type = words[words.length - 1];
-  if (contents.get(1).tagName === "br" || contents.get(1).tagName === "span") {
-    name = words.slice(1, -1).join(" ").trim();
-  } else {
-    name = words.slice(1).join(" ").trim();
+module.exports = class TrainParser {
+  constructor(apiRes) {
+    this.ch = cheerio.load(apiRes.d.result.html, { decodeEntities: true });
+    this.polyline = apiRes.d.result.line[0].points;
   }
 
-  if (name === "") name = undefined;
-
-  if (contents.get(1).tagName == "img") {
-    type = contents.eq(1).attr("alt").trim();
+  run() {
+    this.parseTrainHeader();
+    this.parseTrainStations();
+    this.parseTrainExpiry();
+    processStatement(trainPolyline(this.trainNumber, this.polyline));
   }
 
-  if (contents.get(1).tagName == "ul") {
-    type = [];
-    contents.eq(1).find("li").each((i, li) => {
-      let liC = $(li).contents();
-      let spl = liC.eq(0).text().split(":").map(s => s.trim());
-      type[type.length] = { rel: spl[0], type: (spl[1] && spl[1] !== "") ? spl[1] : liC.eq(1).attr("alt").trim() };
-    });
+  parseTrainHeader() {
+    const $ = this.ch;
+    const header = $("th.title").first();
+    const contents = header.contents();
+    const textNode = contents.eq(0).text();
+    const words = textNode.split(" ").map(w => w.trim());
+    this.trainNumber = parseInt(words[0]);
+    let name;
+    let type = words[words.length - 1];
+    if (contents.get(1).tagName === "br" || contents.get(1).tagName === "span") {
+      name = words.slice(1, -1).join(" ").trim();
+    } else {
+      name = words.slice(1).join(" ").trim();
+    }
+
+    if (name === "") name = undefined;
+
+    if (contents.get(1).tagName == "img") {
+      type = contents.eq(1).attr("alt").trim();
+    }
+
+    if (contents.get(1).tagName == "ul") {
+      type = [];
+      contents.eq(1).find("li").each((i, li) => {
+        let liC = $(li).contents();
+        let spl = liC.eq(0).text().split(":").map(s => s.trim());
+        type[type.length] = {
+          rel: spl[0], type: (spl[1] && spl[1] !== "")
+            ? spl[1] : liC.eq(1).attr("alt").trim()
+        };
+      });
+    }
+
+    let visz = header.find(".viszszam2").text();
+    if (!visz || visz.trim() === "") visz = undefined;
+
+    let spl = header.find("font").text().slice(1, -1).split(",").map(s => s.trim());
+    let relationSpl = spl[0].split(" - ").map(s => s.trim());
+
+    processStatement(trainHeader(this.trainNumber, type, spl[1], { name, visz }));
+    processStatement(trainRelation(this.trainNumber, relationSpl[0], relationSpl[1]));
   }
 
-  let visz = header.find(".viszszam2").text();
-  if (!visz || visz.trim() === "") visz = undefined;
+  parseTrainExpiry() {
+    const $ = this.ch;
 
-  let spl = header.find("font").text().slice(1, -1).split(",").map(s => s.trim());
-  let relationSpl = spl[0].split(" - ").map(s => s.trim());
+    $("div#vt").children("ul").first().find("li")
+      .each((i, li) => {
+        const a = $(li).children().eq(0);
+        if ($(li).attr("style")) {
+          let expiry = a.text().split("-")[1];
+          processStatement(trainExpiry(this.trainNumber, expiry));
+        }
 
-
-  let th = trainHeader(number, type, spl[1], { name, visz });
-  processStatement(trainHeader(number, type, spl[1], { name, visz }));
-  processStatement(trainRelation(number, relationSpl[0], relationSpl[1]));
-  return { ...th, tag: contents.get(1).tagName };
-}
-
-function parseTrainExpiry($) { }
-
-function processPolyline(polyline) {
-  processStatement(trainPolyline(trainNum, polyline));
-}
-
-function parseTrainStations($) {
-  let lastTrainStation = null;
-  $("table.vt tr")
-    .filter((i, tr) => typeof $(tr).attr("class") !== "undefined")
-    .each((i, tr) => {
-      let currentTrainStation = parseTrainStation(tr, $);
-      processStatement(trainStationLink(trainNum, lastTrainStation, currentTrainStation));
-      lastTrainStation = currentTrainStation;
-    });
-
-  processStatement(trainStationLink(trainNum, lastTrainStation, null));
-}
-
-function parseTrainStation(tr, $) {
-  let trainStation = {};
-  $(tr)
-    .children("td")
-    .each((i, td) => {
-      if (i == 0) trainStation.distance = parseInt($(td).text());
-      else if (i == 1) trainStation.name = $(td).text();
-      else if (i == 2) trainStation.arrival = parseTimeTuple($(td).text());
-      else if (i == 3) trainStation.departure = parseTimeTuple($(td).text());
-      else if (i == 4) trainStation.platform = $(td).text();
-    });
-
-  if (trainStation.platform) {
-    let str = trainStation.platform.trim();
-    trainStation.platform = str.length > 0 ? str : null;
+        const onclick = a.attr("onclick");
+        const index = onclick.indexOf("{ v: '") + 6;
+        let elviraId = onclick.slice(index, onclick.indexOf("'", index));
+        processStatement(trainElviraId(this.trainNumber, elviraId));
+      });
   }
 
-  processStatement(
-    trainStationInfo(1, trainStation.name, {
-      distance: trainStation.distance,
-      platform: trainStation.platform,
-      arrival: trainStation.arrival,
-      departure: trainStation.departure
-    })
-  );
+  parseTrainStations() {
+    const $ = this.ch;
+    let lastTrainStation = null;
+    $("table.vt tr")
+      .filter((i, tr) => typeof $(tr).attr("class") !== "undefined")
+      .each((i, tr) => {
+        let currentTrainStation = this.parseTrainStation(tr, $);
+        processStatement(trainStationLink(this.trainNumber, lastTrainStation, currentTrainStation));
+        lastTrainStation = currentTrainStation;
+      });
 
-  return trainStation.name;
-}
+    processStatement(trainStationLink(this.trainNumber, lastTrainStation, null));
+  }
 
-module.exports = { parseTrainStations, parseTrainHeader };
+  parseTrainStation(tr) {
+    const $ = this.ch;
+    let trainStation = {};
+    $(tr)
+      .children("td")
+      .each((i, td) => {
+        if (i == 0) trainStation.distance = parseInt($(td).text());
+        else if (i == 1) trainStation.name = $(td).text();
+        else if (i == 2) trainStation.arrival = parseTimeTuple($(td).text());
+        else if (i == 3) trainStation.departure = parseTimeTuple($(td).text());
+        else if (i == 4) trainStation.platform = $(td).text();
+      });
+
+    if (trainStation.platform) {
+      let str = trainStation.platform.trim();
+      trainStation.platform = str.length > 0 ? str : null;
+    }
+
+    processStatement(
+      trainStationInfo(this.trainNumber, trainStation.name, {
+        distance: trainStation.distance,
+        platform: trainStation.platform,
+        arrival: trainStation.arrival,
+        departure: trainStation.departure
+      })
+    );
+
+    return trainStation.name;
+  }
+};
